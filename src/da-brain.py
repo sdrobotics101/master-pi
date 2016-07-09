2727644ff453294ef472ce3bac63a1d39dfbb6c9
Patrick Paxson <prpaxson@gmail.com>
2016-07-09 22:18:41 +0000

    Update da-brain.py

--- src/da-brain.py
+++ src/da-brain.py
@@ -1,5 +1,29 @@
 from statemachine import StateMachine
 
+def start_transitions(current):
+    return ("Start", current)
+    
+def gatewatch_transitions(current):
+    return ("Gatewatch", current)
+    
+def move1_transitions(current):
+    return ("Move1m", current)
+    
+def visionmove_transitions(current):
+    return ("VisionMove", current)
+    
+def sonarmove_transitions(current):
+    return ("SonarMove", current)
+    
+def avgmove_transitions(current):
+    return ("AverageMove", current)
+    
+def passgate_transitions(current):
+    return ("PassThruGate", current)
+    
+def pathfinder_transitions(current):
+    return ("PathFinder", current)
+
 if __name__== "__main__":
     m = StateMachine()
     m.add_state("Start", start_transitions)
